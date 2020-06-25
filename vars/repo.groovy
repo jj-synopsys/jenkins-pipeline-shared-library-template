8f2995e279d93a9ef9877444abf5099d9bc6603b
jj-synopsys <janesko@synopsys.com>
2020-06-25 19:22:15 +0200

    added bhavins method

--- vars/repo.groovy
+++ vars/repo.groovy
@@ -2,3 +2,7 @@ def getgit(repository) {
   git url: "${repository}"
   sh 'ls -alh'
 }
+
+def checkOutFrom(branch,repo) {    
+  git branch: "${branch}", url: "https://github.com/jj-synopsys/${repo}"   
+}
